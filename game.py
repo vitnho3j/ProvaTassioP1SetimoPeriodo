6543a6d13f87a6f24d6cf529c88e31db50773727
vitnho3j <52510873+vitnho3j@users.noreply.github.com>
2022-04-05 20:42:07 -0300

    Defining constants and methods
    Adicionando constantes para acabar com "número mágicos" e criando alguns métodos

--- game.py
+++ game.py
@@ -1,5 +1,9 @@
 # -*- coding: utf-8 -*-
-const_ = 
+const_Love = 0
+const_Fifteen = 15
+const_Thirty = 30
+const_All = 40
+
 class Game:
     def __init__(self, player1Name, player2Name):
         self.player1Name = player1Name
@@ -12,63 +16,83 @@ class Game:
             self.Player1Score()
         else:
             self.Player2Score()
-    
+
+    def get_Love():
+        return "Love"
+
+    def get_Fifteen():
+        return "Fifteen"
+
+    def get_Thirty():
+        return "Thirty"
+
+
+    def get_Forty():
+        return "Forty"
+
+    def get_All():
+        return "-All"
+
+    def get_Deuce():
+        return "Deuce"
+
     def score(self):
         result = ""
         if (self.p1points == self.p2points and self.p1points < 3):
-            if (self.player1points==0):
-                result = "Love"
-            if (self.player1points==1):
-                result = "Fifteen"
-            if (self.player1points==2):
-                result = "Thirty"
-            result += "-All"
-        if (self.player1points==self.player2points and self.player1points>2):
-            result = "Deuce"
+            if (self.player1points==const_Love):
+                result = self.get_Love()
+            if (self.player1points==const_Fifteen):
+                result = self.get_Fifteen()
+            if (self.player1points==const_Thirty):
+                result = self.get_Thirty()
+            result += self.get_All()
+
+        if (self.player1points==self.player2points and self.player1points > 2):
+            result = self.get_Deuce()
         
         P1res = ""
         P2res = ""
         if (self.player1points > 0 and self.player2points==0):
             if (self.player1points==1):
-                P1res = "Fifteen"
+                P1res = self.get_Fifteen()
             if (self.player1points==2):
-                P1res = "Thirty"
+                P1res = self.get_Thirty()
             if (self.player1points==3):
-                P1res = "Forty"
+                P1res = self.get_Forty()
             
-            P2res = "Love"
+            P2res = self.get_Love()
             result = P1res + "-" + P2res
         if (self.player2points > 0 and self.player1points==0):
             if (self.player2points==1):
-                P2res = "Fifteen"
+                P2res = self.get_Fifteen()
             if (self.player2points==2):
-                P2res = "Thirty"
+                P2res = self.get_Thirty()
             if (self.player2points==3):
-                P2res = "Forty"
+                P2res = self.get_Forty()
             
-            P1res = "Love"
+            P1res = self.get_Love()
             result = P1res + "-" + P2res
         
         
         if (self.player1points>self.player2points and self.player1points < 4):
             if (self.player1points==2):
-                P1res="Thirty"
+                P1res=self.get_Thirty()
             if (self.player1points==3):
-                P1res="Forty"
+                P1res=self.get_Forty()
             if (self.player2points==1):
-                P2res="Fifteen"
+                P2res=self.get_Fifteen()
             if (self.player2points==2):
-                P2res="Thirty"
+                P2res=self.get_Forty()
             result = P1res + "-" + P2res
         if (self.player2points>self.player1points and self.player2points < 4):
             if (self.player2points==2):
-                P2res="Thirty"
+                P2res=self.get_Thirty()
             if (self.player2points==3):
-                P2res="Forty"
+                P2res=self.get_Forty()
             if (self.player1points==1):
-                P1res="Fifteen"
+                P1res=self.get_Fifteen()
             if (self.player1points==2):
-                P1res="Thirty"
+                P1res=self.get_Thirty()
             result = P1res + "-" + P2res
         
         if (self.player1points > self.player2points and self.player2points >= 3):
